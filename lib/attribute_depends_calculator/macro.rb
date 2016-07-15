9be1f7bf1956346a58630cc0ff71a5b603bdb0a4
Jason Hou <hjj1992@gmail.com>
2016-07-15 09:43:43 +0800

    macro method

---
+++ lib/attribute_depends_calculator/macro.rb
@@ -0,0 +1,9 @@
+module AttributeDependsCalculator
+  module Macro
+    def depend(options)
+      options.each do |column, option|
+        Factory.new(self, column, option).perform
+      end
+    end
+  end
+end
