d7de733e9168389ebae2d1e53938a56daf8ad4a6
Jason Hou <hjj1992@gmail.com>
2016-07-16 14:18:08 +0800

    init rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+require "rubygems"
+require "rails"
+require "active_record"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+end
