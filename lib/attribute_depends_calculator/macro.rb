f391a94f5c53e5f8e6aad102984133770b3ceee5
Jason Hou <hjj1992@gmail.com>
2016-07-29 11:32:56 +0800

    alias macro method name

--- lib/attribute_depends_calculator/macro.rb
+++ lib/attribute_depends_calculator/macro.rb
@@ -1,10 +1,13 @@
 module AttributeDependsCalculator
   module Macro
-    def depend(options)
+
+    def attribute_depend(options)
       options.each do |column, option|
         Factory.new(self, column, option).perform
       end
     end
+
+    alias_method :depend, :attribute_depend
   end
 end
 
