d0be1035673f9ae754d931d7e265d2f3881b4d12
Jason Hou <hjj1992@gmail.com>
2016-08-14 08:57:03 +0800

    Add Proc feature to parameter

--- lib/attribute_depends_calculator/parameter.rb
+++ lib/attribute_depends_calculator/parameter.rb
@@ -1,7 +1,7 @@
 module AttributeDependsCalculator
   class Parameter
 
-    attr_accessor :params, :depend_association_name, :depend_column, :operator
+    attr_accessor :params, :depend_association_name, :depend_column, :operator, :expression
 
     OPERATORS = %i(+ *)
     METHODS = %i(sum average count minimum maximum)
@@ -15,20 +15,27 @@ module AttributeDependsCalculator
     def fetch
       self.depend_association_name = params.keys.first
       self.depend_column = params.values.first
-    end
-
-    def expression
       operator_filter
     end
 
     def operator_filter
-      if OPERATORS.include? operator
+      self.expression = if OPERATORS.include? operator
         "pluck(:#{depend_column}).compact.reduce(0, :#{operator})"
       elsif METHODS.include? operator
         "#{operator}(:#{depend_column})"
+      elsif operator.kind_of? Proc
+        @proc = operator
       else
         raise 'The operator of depends calculator are incorrect'
       end
+    end
+
+    def proc?
+      !@proc.nil?
+    end
+
+    def callback
+      @proc
     end
 
   end
