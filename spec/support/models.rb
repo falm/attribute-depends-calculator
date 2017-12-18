29d8e8c75e073c1e21853b04d17442901e761688
Jason Hou <hjj1992@gmail.com>
2017-12-18 09:41:11 +0800

    model calculate

--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,11 +1,15 @@
-class Product < ActiveRecord::Base
-end
+#encoding: utf-8
 
 class OrderItem < ActiveRecord::Base
+  belongs_to :product, class_name: 'Product'
   belongs_to :order
-  belongs_to :product
 end
 
+class Product < ActiveRecord::Base
+
+  has_many :items, class_name: 'OrderItem'
+  depend sells_count: {items: :id}
+end
 
 class Order < ActiveRecord::Base
   has_many :order_items
